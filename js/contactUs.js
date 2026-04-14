58fded9d051a6c5978236257c171ae2f3e9f2a74
CliveC99 <cliveconnell@gmail.com>
2026-04-14 17:52:27 +0100

    Added bootstrap alert messages and add js to hide/show them for contact

--- js/contactUs.js
+++ js/contactUs.js
@@ -23,38 +23,52 @@ document.getElementById("contactForm").addEventListener("submit", function (even
     emailWarning.innerHTML = "";
     descWarning.innerHTML = "";
     successMessage.innerHTML = "";
+    
+    // Hides wanring using d-none class
+    firstNameWarning.classList.add("d-none");
+    surnameWarning.classList.add("d-none");
+    emailWarning.classList.add("d-none");
+    descWarning.classList.add("d-none");
+    successMessage.classList.add("d-none");
 
 
     // If form fields are blank or not selected show the error message until resolved
     if (firstName === "") {
         firstNameWarning.innerHTML = "Enter your first name.";
+        firstNameWarning.classList.remove("d-none");
         isValid = false;
     }
     if (surname === "") {
         surnameWarning.innerHTML = "Enter your surname.";
+        surnameWarning.classList.remove("d-none");
         isValid = false;
     }
     if (email === "") {
         emailWarning.innerHTML = "Enter your email.";
+        emailWarning.classList.remove("d-none");
         isValid = false;
     } else if (!email.includes("@") || !email.includes(".")) {
         emailWarning.innerHTML = "Email must contain an '@' symbol and a full stop '.'";
+        emailWarning.classList.remove("d-none");
         isValid = false;
     }
 
     if (contactReason === "") {
         descWarning.innerHTML = "Enter a description.";
+        descWarning.classList.remove("d-none");
         isValid = false;
     }
 
     if (isValid) {
         successMessage.innerHTML = "Thanks for contacting us!";
+        successMessage.classList.remove("d-none");
 
         /* Help from https://www.w3schools.com/jsref/met_win_setTimeout.asp
         Resets the form and removes the success message after 3 seconds */
         setTimeout(function () {
             document.getElementById("contactForm").reset();
             successMessage.innerHTML = "";
+            successMessage.classList.add("d-none");
         }, 3000);
     }
 })
